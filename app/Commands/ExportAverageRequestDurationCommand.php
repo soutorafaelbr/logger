af5cbba7311b819e7b6f948bc41c37c446a05b70
Rafael Souto <souto.rafaelb@gmail.com>
2020-08-07 13:26:14 -0300

    log-01: rename average request duration command

--- app/Commands/ExportAverageRequestDurationCommand.php
+++ app/Commands/ExportAverageRequestDurationCommand.php
@@ -4,7 +4,7 @@ namespace App\Commands;
 use App\Exports\RequestAverageDurationPerServiceExport;
 use LaravelZero\Framework\Commands\Command;
 
-class ExportAverageRequestDuration extends Command
+class ExportAverageRequestDurationCommand extends Command
 {
     protected $signature = 'export:average-duration-per-service';
 
